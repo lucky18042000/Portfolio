abb812235ec09d2247f9e3073281a26ea64ef0b4
lucky18042000 <rohitsingh182000@gmail.com>
2022-12-31 16:20:32 +0530

    navbar_togglebuttonfuncationallity

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 function Navbar() {
+
+  const [menu,setMenu] = React.useState(false);
+  function handleclick() {
+    setMenu(prevmenu => !prevmenu)
+  }
   return (
     <div>
       <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900  fixed w-full z-20 top-0 left-0  border-gray-200 dark:border-gray-600">
@@ -24,6 +29,7 @@ function Navbar() {
               className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-sticky"
               aria-expanded="false"
+              onClick={handleclick}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -42,7 +48,7 @@ function Navbar() {
             </button>
           </div>
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className={menu ? "items-center justify-between  w-full md:flex md:w-auto md:order-1":"items-center justify-between hidden w-full md:flex md:w-auto md:order-1"}
             id="navbar-sticky"
           >
             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
